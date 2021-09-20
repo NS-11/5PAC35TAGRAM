823e6bef0d097ce0f7f042e5ee786075f6f41f66
NS-11 <noemie.stonge@hotmail.com>
2021-09-20 00:45:03 -0400

    Collapsable description and animated heart

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header() {
                 <img className='Header-image' src={nasaIcon} alt="test"/>
                 <div>
                     <h1>Spacestagram</h1>
-                    <p>Brought to you by NASA's Astronomy Photo of the Day (APOD) API</p>
+                    <p className="Header-subtitle">Brought to you by NASA's Astronomy Photo of the Day (APOD) API</p>
                 </div>
             </div>
         </nav>
--- src/components/Post/Info.js
+++ src/components/Post/Info.js
@@ -1,41 +1,57 @@
 import React from "react"
 import { FiHeart } from 'react-icons/fi'
 import { FaHeart } from 'react-icons/fa';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io/'
+import Bounce from 'react-reveal/Bounce'
 import './post.css'
 
 class Info extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            showDescription: false,
-            liked: false
+            imageLiked: false,
+            showDescription: false
         }
         this.likeButtonClicked = this.likeButtonClicked.bind(this)
+        this.arrowClicked = this.arrowClicked.bind(this)
     }
 
     likeButtonClicked() {
         this.setState(prevState => {    
             return {
-                liked: !prevState.liked
+                imageLiked: !prevState.imageLiked
+            }
+        })
+    }
+
+    arrowClicked() {
+        this.setState(prevState => {
+            return {
+                showDescription: !prevState.showDescription
             }
         })
     }
 
     render(props) {
         const post = this.props.post
-        const imageLiked = this.state.liked ? <FaHeart size="30px" color="hotpink" onClick={this.likeButtonClicked}/> : <FiHeart size="30px" color="hotpink" onClick={this.likeButtonClicked}/>
+        const { imageLiked, showDescription } = this.state
+        
         return (
             <div className="Post-info">
-                <div className="Post-title">
-                        <h3>{post.title}</h3>
+                <div className="Post-header">
+                        <h3 className="Post-title">{post.title}</h3>
                         <p>{post.date}</p>                    
                 </div>
-                <p>
-                    {post.explanation}
-                </p>
-                <div>
-                {imageLiked}
+                <br/>
+                {showDescription && <p>{post.explanation}</p>}
+                <div className="Post-arrow">
+                    {!showDescription && <IoIosArrowDown size="20px" onClick={this.arrowClicked}/>}
+                    {showDescription && <IoIosArrowUp size="20px" onClick={this.arrowClicked}/>}
                 </div>
+                {!imageLiked && <FiHeart id="Post-heart-like" className="Post-heart" onClick={this.likeButtonClicked}/>}
+                <Bounce when={imageLiked}>
+                    {imageLiked && <FaHeart className="Post-heart" onClick={this.likeButtonClicked}/>}
+                </Bounce>
             </div>
         )
     }
